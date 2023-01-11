c21bc53958af50c865850ee24a59f42355e0857c
Weslley Moura <schno@WeslleyMoura.localdomain>
2023-01-11 11:44:51 -0300

    Resolvendo requisitos 9 e acima

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
     const btnRandomColors = document.getElementById('button-random-color');
+    const btnClear = document.getElementById('clear-board');
     let colorsPalette = document.getElementById('color-palette');
+    const block1 = document.getElementById('block1')
     const block2 = document.getElementById('block2');
     const block3 = document.getElementById('block3');
     const block4 = document.getElementById('block4');
-    const colors = document.getElementsByClassName('color');
+    const colors = document.querySelectorAll('.color');
+    block1.classList.add('selected');
+    
 
-    const save = () => {
-        const paleta = document.querySelector('#color-palette');        
-        localStorage.setItem('colorPalette', paleta.innerHTML);
+    const save = () => {          
+        localStorage.setItem('colorPalette', JSON.stringify([block1.style.backgroundColor, block2.style.backgroundColor, block3.style.backgroundColor, block4.style.backgroundColor]));        
     }
 
-    btnRandomColors.addEventListener('click', () => {        
+    const recoveryObject = JSON.parse(localStorage.getItem('colorPalette'));
+    for(let index = 0; index < colors.length; index += 1){
+        if(localStorage.getItem('colorPalette') != null){
+            for(index = 0; index <recoveryObject.length; index += 1) {
+                colors[index].style.backgroundColor = recoveryObject[index];
+            }
+        }
+    }
+
+    btnRandomColors.addEventListener('click', () => {            
         const hex1 = (Math.random()*0xFFFFFF<<0).toString(16);
         const hex2 = (Math.random()*0xFFFFFF<<0).toString(16);
         const hex3 = (Math.random()*0xFFFFFF<<0).toString(16);
+        let color1 = '#000000';
         let color2 = `#${hex1}`;
         let color3 = `#${hex2}`;
         let color4 = `#${hex3}`;
+        block1.style.backgroundColor = color1;
         block2.style.backgroundColor = color2;
         block3.style.backgroundColor = color3;
         block4.style.backgroundColor = color4;                
         save()        
     })
-    
-
     const generateCells = () => {
         const matriz = document.querySelector('#pixel-board');        
         for (let index = 0; index < 5; index += 1){
@@ -31,15 +43,48 @@
             line.className = 'line';
             for(let index1 = 0; index1 <5; index1 += 1){
                 const cell = document.createElement('div');
-                cell.className = 'pixel';
+                cell.className = 'pixel';     
+                cell.addEventListener('click', () => {
+                    let target = document.querySelector('.selected')
+                    let targetColor = target.style.backgroundColor;
+                    cell.style.backgroundColor = targetColor
+                })           
                 line.appendChild(cell);
             }
             matriz.appendChild(line);
         }        
     }
     generateCells()
+    
+    const generateEventClick = () => {
+        for(index = 0; index < colors.length; index += 1){
+            colors[index].addEventListener('click', (event) =>{
+                if (event.target === document.querySelector('.selected')){
+                    targetColor = target.style.backgroundColor;
+                }
+                if (event.target != document.querySelector('.selected')){
+                    for(index = 0; index < colors.length; index +=1){
+                        if(colors[index] === document.querySelector('.selected')){
+                            colors[index].classList.remove('selected');
+                        }
+                    }
+                    event.target.classList.add('selected');
+                    
+                }
+                targetColor = target.style.backgroundColor;
+            })
+        }
+    }
+    generateEventClick();
+    
+    btnClear.addEventListener('click', () => {
+        pixelClass = document.querySelectorAll('.pixel');
+        for (index = 0; index < pixelClass.length; index += 1){
+            pixelClass[index].style.backgroundColor = 'white';
+        }
+    })
 
-   
+      
     
     
 
